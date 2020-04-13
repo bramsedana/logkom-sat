f93e21a1ae8e1f5ad498d3f1961b6381942127bf
Michael Giorgio <michaelgwirawan@gmail.com>
2020-04-13 19:57:11 +0800

    1. Deleted unused files 2. Update requirements

--- algorithm.py
+++
@@ -1,6 +0,0 @@
-input = [[0,1],[1,0]]
-
-n = len(input) if (isinstance(input, list) and isinstance(input[0], list) and (len(input) == len(input[0]))) else 0
-
-# if (n not 0):
-# minisat? library? atau for for aja?
