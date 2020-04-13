9c27e0796ad5836f2af2770f8bf3f7490fa9645f
Rahmania Astrid Mochtar <rahmania.astrid@ui.ac.id>
2020-04-13 21:15:44 +0700

    add button functionality

--- gui.py
+++ gui.py
@@ -16,10 +16,9 @@ from PyQt5.QtGui import QPixmap
 from PyQt5 import QtWidgets, QtCore
 from PyQt5.QtCore import pyqtSlot
 
-from be.latin_square import *
+# from be.latin_square import *
 
 ERROR_MSG = "ERROR"
-
 
 # Create a subclass of QMainWindow to setup the calculator's GUI
 class PyCalcUi(QMainWindow):
@@ -39,7 +38,6 @@ class PyCalcUi(QMainWindow):
         self.pushButton = QtWidgets.QPushButton(self._centralWidget)
 
         # Create the display and the buttons
-        # self._createDisplay()
         self._createLabels()
         self._takeinputs()
         self._createButtons()
@@ -48,7 +46,8 @@ class PyCalcUi(QMainWindow):
 
     @pyqtSlot()
     def on_submit_click(self):
-        result = lat_square_sat([[1,2,3],[3,1,2],[2,3,1]])
+        # result = lat_square_sat([[1,2,3],[3,1,2],[2,3,1]])
+        result = True
         print('PyQt5 button click submit')
         print("Result from BE {}".format(result))
         resultString = "YOU \nWIN" if result else "YOU \nLOSE:("
@@ -97,17 +96,6 @@ class PyCalcUi(QMainWindow):
              self.noticeLabel.setText('Latin Square Succesfully Initialized with\nSize: ' + str(self.n))
              self.pushButton.hide()
 
-    # def _createDisplay(self):
-    #     """Create the display."""
-    #     # Create the display widget
-    #     self.display = QLineEdit()
-    #     # Set some display's properties
-    #     self.display.setFixedHeight(35)
-    #     self.display.setAlignment(Qt.AlignRight)
-    #     self.display.setReadOnly(True)
-    #     # Add the display to the general layout
-    #     self.generalLayout.addWidget(self.display)
-
     def _createButtons(self):
         """Create the buttons."""
         self.buttons = {}
@@ -128,70 +116,45 @@ class PyCalcUi(QMainWindow):
         for btnText, pos in buttons.items():
             self.buttons[btnText] = QPushButton("")
             self.buttons[btnText].setFixedSize(60, 60)
-            self.buttons[btnText].setIcon(QIcon(QPixmap("assets/2-charmander.png")))
             # self.buttons[btnText].setIconSize()
             buttonsLayout.addWidget(self.buttons[btnText], pos[0], pos[1])
         # Add buttonsLayout to the general layout
         self.generalLayout.addLayout(buttonsLayout)
-
-    def setDisplayText(self, text):
-        """Set display's text."""
-        self.display.setText(text)
-        self.display.setFocus()
-
-    def displayText(self):
-        """Get display's text."""
-        return self.display.text()
-
-    def clearDisplay(self):
-        """Clear the display."""
-        self.setDisplayText("")
-
-
-# Create a Model to handle the calculator's operation
-def evaluateExpression(expression):
-    """Evaluate an expression."""
-    try:
-        result = str(eval(expression, {}, {}))
-    except Exception:
-        result = ERROR_MSG
-
-    return result
 
 
 # Create a Controller class to connect the GUI and the model
 class PyCalcCtrl:
     """PyCalc's Controller."""
 
-    def __init__(self, model, view):
+    def __init__(self, view):
         """Controller initializer."""
-        self._evaluate = model
         self._view = view
+        self._counter = 1
+        self._pushedBtn = []
         # Connect signals and slots
         self._connectSignals()
 
-    def _calculateResult(self):
-        """Evaluate expressions."""
-        result = self._evaluate(expression=self._view.displayText())
-        self._view.setDisplayText(result)
+    def _changeIcon(self, btn, btnText):
+        """Change image of button."""
+        if btnText not in self._pushedBtn:
+            if self._counter == 1:
+                btn.setIcon(QIcon(QPixmap("assets/1-turtwig.png")))
+            elif self._counter == 2:
+                btn.setIcon(QIcon(QPixmap("assets/2-charmander.png")))
+            elif self._counter == 3:
+                btn.setIcon(QIcon(QPixmap("assets/3-mudkip.png")))
 
-    def _buildExpression(self, sub_exp):
-        """Build expression."""
-        if self._view.displayText() == ERROR_MSG:
-            self._view.clearDisplay()
+            if self._counter == 3:
+                self._counter = 1
+            else:
+                self._counter += 1
 
-        expression = self._view.displayText() + sub_exp
-        self._view.setDisplayText(expression)
+            self._pushedBtn.append(btnText)
 
     def _connectSignals(self):
         """Connect signals and slots."""
         for btnText, btn in self._view.buttons.items():
-            if btnText not in {"=", "C"}:
-                btn.clicked.connect(partial(self._buildExpression, btnText))
-
-        # self._view.buttons["="].clicked.connect(self._calculateResult)
-        # self._view.display.returnPressed.connect(self._calculateResult)
-        # self._view.buttons["C"].clicked.connect(self._view.clearDisplay)
+            btn.clicked.connect(partial(self._changeIcon, btn, btnText))
 
 
 # Client code
@@ -203,8 +166,7 @@ def main():
     view = PyCalcUi()
     view.show()
     # Create instances of the model and the controller
-    model = evaluateExpression
-    PyCalcCtrl(model=model, view=view)
+    PyCalcCtrl(view=view)
     # Execute calculator's main loop
     sys.exit(pycalc.exec_())
 
