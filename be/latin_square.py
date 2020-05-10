9948e775cdfe58d2499e0c0c5454552a7f850c40
bramsedana <wehantouw@gmail.com>
2020-05-10 18:10:19 +0700

    Update latin_square.py

--- be/latin_square.py
+++ be/latin_square.py
@@ -30,27 +30,26 @@ def lat_square_sat(mat):
                 else:
                     assumption.append(-var1)
 
-            clause4 = []
-            clause5 = []
             for a in range(size-1):
+                var1 = construct_var(a,x,y)
+                var3 = construct_var(x,a,y)
                 for b in range(a+1, size):
-                    var1 = construct_var(a,x,y)
+                    clause4 = []
+                    clause5 = []
                     var2 = construct_var(b,x,y)
-                    var3 = construct_var(x,a,y)
                     var4 = construct_var(x,b,y)
-
                     clause4.append(-var1)
                     clause4.append(-var2)
                     clause5.append(-var3)
                     clause5.append(-var4)
+                    g.add_clause(clause4)
+                    g.add_clause(clause5)
+                    print(f"clause4 {clause4}")
+                    print(f"clause5 {clause5}")
 
             g.add_clause(clause1)
             g.add_clause(clause2)
             g.add_clause(clause3)
-            g.add_clause(clause4)
-            g.add_clause(clause5)
-            print(f"clause4 {clause4}")
-            print(f"clause5 {clause5}")
     #         print(f"clause1 {clause1}")
     #         print(f"clause2 {clause2}")
     #         print(f"clause3 {clause3}")
